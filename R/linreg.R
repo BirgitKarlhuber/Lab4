a3ea49fcaa7b9d1bcfc39e5ac6bf23b686b1696f
BirgitKarlhuber <birgit@karlhuber.net>
2023-09-15 21:11:21 +0200

    set up new package

---
+++ R/linreg.R
@@ -0,0 +1,21 @@
+#' Algorithm to find the shortest path from an initial node to all other nodes in a graph
+#' 
+#' The algorithm takes a graph and an initial node and calculates the shortest path from the initial node to every other node in the graph.
+#'
+#' @param graph data.frame with three variables (v1, v2 and w) that contains the 
+#' edges of the graph (from v1 to v2) with the weight of the edge (w).
+#' 
+#' @param init_node A number corresponding to the start node
+#'
+#' @return The shortest path to every other node from the starting node as a vector.
+#' 
+#' @export
+#' 
+#' 
+#' @seealso \url{https://en.wikipedia.org/wiki/Dijkstra’s_algorithm}
+
+
+linreg <- function(){
+  
+}
+
